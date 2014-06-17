46faff18d12b9a2e79b7f635a9d950fe241077a8
Steven Evans <kopelli@gmail.com>
2014-06-16 21:17:50 -0400

    Updating the output from the installer

--- install.bat
+++ install.bat
@@ -7,7 +7,7 @@ echo "Run this script from 'cmd' and not from bash!"
 exit $?
 
 :CMDSCRIPT
-echo Welcome to %COMSPEC%
+REM echo Welcome to %COMSPEC%
 
 setlocal enabledelayedexpansion
 
@@ -16,12 +16,12 @@ cd %~dp0
 for /F "usebackq delims==" %%f IN (`dir /b "_*"`) DO (
     SET _dot_file_=%%f
     SET _dot_file_=.!_dot_file_:~1!
-    echo %USERPROFILE%\!_dot_file_!
+REM    echo %USERPROFILE%\!_dot_file_!
     FOR /F "usebackq delims==" %%g IN (`dir /b /s "%%f"`) DO (
         SET _full_path_=%%g
     )
     del /q /f %USERPROFILE%\!_dot_file_!
-    mklink /h %USERPROFILE%\!_dot_file_! !_full_path_!
+    mklink %USERPROFILE%\!_dot_file_! !_full_path_!
 )
 :: --delete actual file if it exists
 :: --mklink <Full path for link file> <full path of %%f>
