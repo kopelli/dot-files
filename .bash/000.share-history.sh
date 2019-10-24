3588aa9eb883e5e97a6d5871035feadcb9341cd3
Steven Evans <kopelli@gmail.com>
2019-10-24 09:48:02 -0400

    extract shared history

---
+++ .bash/000.share-history.sh
@@ -0,0 +1,15 @@
+# Set the 'history' command to show when the previous commands were executed
+HISTTIMEFORMAT="[%F %T] "
+
+# Append history across sessions
+shopt -s histappend
+
+# Verify that history can be appended, or recover if something goes wrong
+shopt -s histreedit
+shopt -s histverify
+
+# Ignore history commands that start with whitespace or duplicate commands
+HISTCONTROL='ignoreboth'
+
+# Commit history for the window, clear history, and reload all history before showing the prompt
+PROMPT_COMMAND="history -a;history -c;history -r; $PROMPT_COMMAND"
