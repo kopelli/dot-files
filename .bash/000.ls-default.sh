6c4655971b0c47c81502a607d5c612f91f38b0a0
Steven Evans <kopelli@gmail.com>
2019-10-23 22:17:25 -0400

    move ls override to its own file

---
+++ .bash/000.ls-default.sh
@@ -0,0 +1,11 @@
+# Modifying this from https://stackoverflow.com/a/1688464
+if ls --color -d . >/dev/null 2>&1; then
+  LS_SUPPORT=gnu
+  alias ls='ls -l -A -p -G -v --color'
+elif ls -G -d . >/dev/null 2>&1; then
+  LS_SUPPORT=bsd
+  alias ls='ls -l -A -p -G -v'
+else
+  LS_SUPPORT=solaris
+  alias ls='ls -l -A'
+fi
