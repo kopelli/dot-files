c33ed4ca51c2822a6265b7b8412d53b46200f875
Steven Evans <kopelli+github@gmail.com>
2020-01-28 09:45:44 -0600

    Remove TODO comment
    This has been moved to issue #14

--- .bash/100.command_not_found_handle.sh
+++ .bash/100.command_not_found_handle.sh
@@ -1,7 +1,6 @@
 func_exists=`declare -f -F command_not_found_handle`
 if [[ -n "$func_exists" ]]; then
   command_not_found_handle() {
-    # dig into https://github.com/search?q=command_not_found_handle&type=Code for further ideas
     printf "%s: command not found!!!!!!!\n" "$1" >&2
     return 127
   }
