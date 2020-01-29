faec468011883f9ab82efdef339e3a7bec4ae641
Steven Evans <kopelli@gmail.com>
2020-01-29 12:39:25 -0600

    Make modifying PROMPT_COMMAND idempotent

--- .bash/000.share-history.sh
+++ .bash/000.share-history.sh
@@ -11,5 +11,7 @@ shopt -s histverify
 # Ignore history commands that start with whitespace or duplicate commands
 HISTCONTROL='ignoreboth'
 
-# Commit history for the window, clear history, and reload all history before showing the prompt
-PROMPT_COMMAND="history -a;history -c;history -r; $PROMPT_COMMAND"
+if [[ $PROMPT_COMMAND != *"history -c;"* ]]; then
+    # Commit history for the window, clear history, and reload all history before showing the prompt
+    PROMPT_COMMAND="history -a;history -c;history -r;$PROMPT_COMMAND"
+fi
