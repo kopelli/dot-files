0107b6df16d9f532abbed7bdb4f556d8b5f9d8b6
Steven Evans <kopelli@gmail.com>
2020-02-10 11:33:45 -0600

    Reset Windows batch file to core template

--- install.bat
+++ install.bat
@@ -1,29 +1,51 @@
 :<<"::SHELLSCRIPT"
-@echo off
-GOTO :CMDSCRIPT
+@GOTO :CMDSCRIPT
 
 ::SHELLSCRIPT
 echo "Run this script from 'cmd' and not from bash!"
 exit $?
 
 :CMDSCRIPT
-REM echo Welcome to %COMSPEC%
+@pushd .
+:: Don't let our script variables leak beyond our script
+@setlocal enableextensions enabledelayedexpansion
+@SET SCRIPT="%~dpnx0"
+@SET SCRIPT_IS_INTERACTIVE=1
 
-setlocal enabledelayedexpansion
+:: Change the working directory to the directory where the script lives
+@cd /d "%~dp0"
 
-pushd .
-cd %~dp0
-for /F "usebackq delims==" %%f IN (`dir /b "_*"`) DO (
-    SET _dot_file_=%%f
-    SET _dot_file_=.!_dot_file_:~1!
-REM    echo %USERPROFILE%\!_dot_file_!
-    FOR /F "usebackq delims==" %%g IN (`dir /b /s "%%f"`) DO (
-        SET _full_path_=%%g
-    )
-    del /q /f %USERPROFILE%\!_dot_file_!
-    mklink %USERPROFILE%\!_dot_file_! !_full_path_!
+:: Search for the script's name in the command's command line,
+:: if the script exists there, it liely was executed by Explorer rather than the command line
+:: Inspired by https://stackoverflow.com/a/9422268
+@echo %cmdcmdline% | FIND /i %SCRIPT% >nul
+@IF NOT errorlevel 1 SET SCRIPT_IS_INTERACTIVE=0
+
+:: Check if the script is running as admin
+@NET Session >nul 2>&1
+@IF %ERRORLEVEL% == 0 (
+  :: Running as admin
+  echo "Okay admin..."
+) ELSE (
+  :: Not admin...
+  echo Sorry lowly user...
 )
+
+::for /F "usebackq delims==" %%f IN (`dir /b "_*"`) DO (
+::    SET _dot_file_=%%f
+::    SET _dot_file_=.!_dot_file_:~1!
+::REM    echo %USERPROFILE%\!_dot_file_!
+::    FOR /F "usebackq delims==" %%g IN (`dir /b /s "%%f"`) DO (
+::        SET _full_path_=%%g
+::    )
+::    del /q /f %USERPROFILE%\!_dot_file_!
+::    mklink %USERPROFILE%\!_dot_file_! !_full_path_!
+::)
 :: --delete actual file if it exists
 :: --mklink <Full path for link file> <full path of %%f>
-popd
-endlocal
+::
+:EXIT
+:: Pause only if the script was opened from Explorer
+@IF "%SCRIPT_IS_INTERACTIVE%"=="0" PAUSE
+@ENDLOCAL
+@POPD
