f667c91639dedf44aa74f5015450d1b8605943cd
Steven Evans <kopelli@gmail.com>
2020-02-13 15:55:01 -0600

    Perform chocolatey update if it is already installed

--- tools/powershell/choco.ps1
+++ tools/powershell/choco.ps1
@@ -1,5 +1,11 @@
-$Script:CurrentExecutionPolicy = Get-ExecutionPolicy
-Set-ExecutionPolicy Bypass -Scope Process -Force
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-Set-ExecutionPolicy $Script:CurrentExecutionPolicy -Scope Process -Force
+Get-Command choco -ErrorVariable ChocoError -ErrorAction SilentlyContinue | Out-Null
+if ($ChocoError) {
+    # Chocolatey is not installed (or at least can't be found), so install it
+    $Script:CurrentExecutionPolicy = Get-ExecutionPolicy
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    Set-ExecutionPolicy $Script:CurrentExecutionPolicy -Scope Process -Force
+} else {
+    & choco upgrade all --yes
+}
