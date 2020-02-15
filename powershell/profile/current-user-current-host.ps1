fbfb09d655589525b89b9e257a19a01168a8965d
Steven Evans <kopelli@gmail.com>
2020-02-14 22:39:36 -0600

    Update Powershell prompt
    Attempt to bring it in line with the Bash prompt

--- powershell/profile/current-user-current-host.ps1
+++ powershell/profile/current-user-current-host.ps1
@@ -5,3 +5,34 @@ $Script:ChocolatelyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.p
 if (Test-Path($Script:ChocolatelyProfile)) {
     Import-Module $Script:ChocolatelyProfile
 }
+
+# Define the custom prompt
+Function prompt {
+    $ESC = [char]27
+    $color_reset = "$ESC[m"
+    $color_red = "$ESC[1;31m"
+    $color_green = "$ESC[1;32m"
+    $color_yellow = "$ESC[1;33m"
+    $color_blue = "$ESC[1;34m"
+    $color_cyan = "$ESC[1;36m"
+    $color_white = "$ESC[1;37m"
+    $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+    $principal = [Security.Principal.WindowsPrincipal] $identity
+
+    $prepend = ""
+    if (Test-Path variable:/PSDebugContext) {
+        $prepend = "$color_green[DBG]: $color_reset"
+    } elseif ($principal.IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
+        $prepend = "$color_red[ADMIN]: $color_reset"
+    }
+
+    # Final prompt output
+    $prepend `
+    + $color_blue + $env:USERNAME + $color_reset `
+    + "@" `
+    + $color_cyan + $env:COMPUTERNAME + $color_reset `
+    + " [" `
+    + $color_green + $(Get-Location) + $color_reset `
+    + "]`n" `
+    + $(if ($NestedPromptLevel -ge 1) { ">>" }) + "> "
+}
