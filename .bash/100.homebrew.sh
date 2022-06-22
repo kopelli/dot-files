811b8925e3ead60eeee8553d069566287c14cd56
Steven Evans <kopelli+github@gmail.com>
2022-06-22 09:09:46 -0500

    fix(homebrew): force GNU Find to take precedence over system tool

--- .bash/100.homebrew.sh
+++ .bash/100.homebrew.sh
@@ -6,4 +6,12 @@ if [[ -d "$_path" ]]; then
     fi
 fi
 
+_path=/usr/local/opt/findutils/libexec/gnubin
+
+if [[ -d "$_path" ]]; then
+    if [[ ":$PATH" != *":$_path:"* ]]; then
+        export PATH="$_path:$PATH"
+    fi
+fi
+
 unset _path
