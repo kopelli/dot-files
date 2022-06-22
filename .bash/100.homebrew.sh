02f17684cf840897e40d9f86473541d9986d481b
Steven Evans <kopelli+github@gmail.com>
2022-06-22 09:08:21 -0500

    fix(homebrew): change PATH to access Homebrew tools first

--- .bash/100.homebrew.sh
+++ .bash/100.homebrew.sh
@@ -2,7 +2,7 @@ _path=/home/linuxbrew/.linuxbrew/bin
 
 if [[ -d "$_path" ]]; then
     if [[ ":$PATH" != *":$_path:"* ]]; then
-        export PATH="$PATH:$_path"
+        export PATH="$_path:$PATH"
     fi
 fi
 
