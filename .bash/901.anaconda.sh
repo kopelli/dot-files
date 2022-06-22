4206354fadea1c1830d373351c8eba7a4395555f
Steven Evans <kopelli+github@gmail.com>
2022-06-22 09:25:37 -0500

    feat(bash): support for Anaconda3

---
+++ .bash/901.anaconda.sh
@@ -0,0 +1,13 @@
+# _!!_ Contents within this block are managed by 'conda init' _!!_
+__conda_setup="$('$HOME/opt/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "$HOME/opt/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "$HOME/opt/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/Users/scevans/opt/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+
