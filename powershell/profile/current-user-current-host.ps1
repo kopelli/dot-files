8e04f3ab235f5835849dab4b756b24c6568c0da2
Steven Evans <kopelli@gmail.com>
2023-06-12 13:39:58 -0500

    pwsh: Starship pre-command to reopen to a current directory

--- powershell/profile/current-user-current-host.ps1
+++ powershell/profile/current-user-current-host.ps1
@@ -62,4 +62,13 @@ Function prompt {
 Get-Command -ErrorAction SilentlyContinue -ErrorVariable StarshipExists starship | Out-Null
 if ("$StarshipExists" -eq "") {
     Invoke-Expression (&starship init powershell)
+    function Invoke-Starship-PreCommand {
+      $loc = $executionContext.SessionState.Path.CurrentLocation;
+      $prompt = "$([char]27)]9;12$([char]7)"
+      if ($loc.Provider.Name -eq "FileSystem")
+      {
+        $prompt += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+      }
+      $host.ui.Write($prompt)
+    }
 }
