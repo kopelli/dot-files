098ef2bc0ca702784c4c6af9b4cb980f8a400ab9
Steven Evans <129903750+sevans-tcp@users.noreply.github.com>
2023-07-17 11:25:48 -0500

    format(powershell): Align formatting with PSScriptAnalyzer settings

--- powershell/profile/current-user-current-host.ps1
+++ powershell/profile/current-user-current-host.ps1
@@ -1,15 +1,15 @@
 Function DirectoryIsInPath {
-	param(
-		[string] $path
-	)
+    param(
+        [string] $path
+    )
 
-	if ( (Test-Path($path)) -and -not ($env:PATH -match [regex]::Escape($path))) {
-		$env:PATH="$path;$env:PATH"
-	}
+    if ( (Test-Path($path)) -and -not ($env:PATH -match [regex]::Escape($path))) {
+        $env:PATH = "$path;$env:PATH"
+    }
 }
 
-DirectoryIsInPath(Join-Path $env:ProgramData "bin")
-DirectoryIsInPath(Join-Path $env:LocalAppData "Keybase")
+DirectoryIsInPath(Join-Path $env:ProgramData 'bin')
+DirectoryIsInPath(Join-Path $env:LocalAppData 'Keybase')
 Remove-Item -Path Function:DirectoryIsInPath
 
 $Script:ChocolatelyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
@@ -28,21 +28,21 @@ Register-ArgumentCompleter -Native -CommandName aws -ScriptBlock {
 
     $env:COMP_LINE = $wordToComplete
     if ($env:COMP_LINE.length -lt $cursorPosition) {
-	    $env:COMP_LINE = $env:COMP_LINE + " "
+        $env:COMP_LINE = $env:COMP_LINE + ' '
     }
     $env:COMP_POINT = $cursorPosition
     aws_completer.exe | ForEach-Object {
-	    [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
     }
     Remove-Item Env:\COMP_LINE
     Remove-Item Env:\COMP_POINT
 }
 
 # Configure XDG variables...mainly for nvim
-$env:XDG_CONFIG_HOME=(Join-Path $env:USERPROFILE ".config")
-$env:XDG_DATA_HOME=$env:LOCALAPPDATA
+$env:XDG_CONFIG_HOME = (Join-Path $env:USERPROFILE '.config')
+$env:XDG_DATA_HOME = $env:LOCALAPPDATA
 # XDG_RUNTIME_DIR is fine to stay in TMP
-$env:XDG_STATE_HOME=$env:LOCALAPPDATA
+$env:XDG_STATE_HOME = $env:LOCALAPPDATA
 
 # Define the custom prompt
 Function prompt {
@@ -57,34 +57,34 @@ Function prompt {
     $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
     $principal = [Security.Principal.WindowsPrincipal] $identity
 
-    $prepend = ""
+    $prepend = ''
     if (Test-Path variable:/PSDebugContext) {
         $prepend = "$color_green[DBG]: $color_reset"
-    } elseif ($principal.IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
+    }
+    elseif ($principal.IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
         $prepend = "$color_red[ADMIN]: $color_reset"
     }
 
     # Final prompt output
     $prepend `
-    + $color_blue + $env:USERNAME + $color_reset `
-    + "@" `
-    + $color_cyan + $env:COMPUTERNAME + $color_reset `
-    + " [" `
-    + $color_green + $(Get-Location) + $color_reset `
-    + "]`n" `
-    + $(if ($NestedPromptLevel -ge 1) { ">>" }) + "> "
+        + $color_blue + $env:USERNAME + $color_reset `
+        + '@' `
+        + $color_cyan + $env:COMPUTERNAME + $color_reset `
+        + ' [' `
+        + $color_green + $(Get-Location) + $color_reset `
+        + "]`n" `
+        + $(if ($NestedPromptLevel -ge 1) { '>>' }) + '> '
 }
 
 Get-Command -ErrorAction SilentlyContinue -ErrorVariable StarshipExists starship | Out-Null
-if ("$StarshipExists" -eq "") {
+if ("$StarshipExists" -eq '') {
     Invoke-Expression (&starship init powershell)
     function Invoke-Starship-PreCommand {
-      $loc = $executionContext.SessionState.Path.CurrentLocation;
-      $prompt = "$([char]27)]9;12$([char]7)"
-      if ($loc.Provider.Name -eq "FileSystem")
-      {
-        $prompt += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
-      }
-      $host.ui.Write($prompt)
+        $loc = $executionContext.SessionState.Path.CurrentLocation
+        $prompt = "$([char]27)]9;12$([char]7)"
+        if ($loc.Provider.Name -eq 'FileSystem') {
+            $prompt += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+        }
+        $host.ui.Write($prompt)
     }
 }
