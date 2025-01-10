80966c8427f90ee2c010c44cf2c0da73f8e26c20
Steven Evans <129903750+sevans-tcp@users.noreply.github.com>
2025-01-10 16:28:28 -0600

    nvim: start personalizing config

---
+++ .vim/init.lua
@@ -0,0 +1,18 @@
+-- Disallow the use of :autocmd, shell, and write commands
+vim.opt.secure = true
+
+-- Always show line numbers
+vim.opt.ruler = true
+
+-- Switch to relative line numbering to make it easier to jump up/down lines
+vim.opt.relativenumber = true
+
+-- Set the height of the command line
+vim.opt.cmdheight = 2
+
+-- Ignore case when doing searches
+vim.opt.ignorecase = true
+
+-- When searching, try to be smart about cases
+vim.opt.smartcase = true
+
