19f9d372de343bbd2517914ae4aebce9ee2652ee
Steven Evans <129903750+sevans-tcp@users.noreply.github.com>
2025-01-10 16:23:19 -0600

    fix alias; Pwsh functions

--- powershell/profile/current-user-current-host.ps1
+++ powershell/profile/current-user-current-host.ps1
@@ -4,7 +4,7 @@ Function DirectoryIsInPath {
     )
 
     if ( (Test-Path($path)) -and -not ($env:PATH -match [regex]::Escape($path))) {
-        $env:PATH = "$path;$env:PATH"
+        $env:PATH = "$path$([System.IO.Path]::PathSeparator)$env:PATH"
     }
 }
 
@@ -88,3 +88,33 @@ if ("$StarshipExists" -eq '') {
         $host.ui.Write($prompt)
     }
 }
+Function Start-Solution() {
+       param()
+
+       $private:SolutionFiles = Get-ChildItem -Path . -Recurse -Depth 3 *.sln
+       if ($private:SolutionFiles -is [array]) {
+               $private:Choices = $private:SolutionFiles | ForEach-Object -Begin { $private:index = 65 } -Process {
+                       $private:file = $_
+                       $choice = New-Object System.Management.Automation.Host.ChoiceDescription "&$([char]$private:index) - $(Resolve-Path -Path $private:file -Relative)", $private:file.FullName
+                       $choice | Add-Member -MemberType NoteProperty -Name FullName -Value ${private:file}.FullName -Force
+                       $private:index += 1
+                       $choice
+               }
+               $private:selection = $host.ui.PromptForChoice("", "Which of these files do you want to open?", $private:choices, -1)
+               Start-Process -FilePath $private:choices[$private:selection].FullName
+       } else {
+               Start-Process -FilePath $private:SolutionFiles
+       }
+}
+
+Set-Alias -Name sln -Value Start-Solution
+
+Function QuitReplacement {
+       Invoke-Command -ScriptBlock { exit }
+}
+Set-Alias -Name Quit -Value QuitReplacement
+
+
+Import-Module posh-sshell
+Start-SshAgent -Quiet
+Add-SshAlias
